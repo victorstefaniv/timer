fd53459efb54a36c3f14fe6afd8bda39566e6f26
Victor Stefaniv <victorstefaniv@gmail.com>
2016-05-13 19:46:14 +0300

    Moved some objects to html, styles to css, some functions shortened

--- timer.js
+++ timer.js
@@ -1,221 +1,143 @@
-var body=document.getElementsByTagName('body')[0],
-	requestText=document.createTextNode('Введіть точку початку зворотнього відліку:'),
-	requestParagraph=body.appendChild(document.createElement('p')),
-	minutesField=body.appendChild(document.createElement('input')),
-	minutesUnit=body.appendChild(document.createElement('div')),
-	secondsField=body.appendChild(document.createElement('input')),
-	secondsUnit=body.appendChild(document.createElement('div')),
-	minutesUnitText=document.createTextNode('хвилин'),
-	secondsUnitText=document.createTextNode('секунд'),
+var body = document.getElementsByTagName('body')[0],
+minutesField = document.getElementById('minutesField'),
+secondsField = document.getElementById('secondsField'),
+startButton=document.getElementById('startButton'),
+stopButton=document.getElementById('stopButton'),
+continueButton=document.getElementById('continueButton'),
+clearButton=document.getElementById('clearButton'),
+startButtonText=document.getElementById('startButton'),
+stopButtonText=document.getElementById('stopButton'),
+continueButtonText=document.getElementById('continueButton'),
+clearButtonText=document.getElementById('clearButton'),
+timerBody=document.getElementById('timerBody'),
+timerText=document.getElementById('timerText'),
+boomSound=document.getElementsByTagName('audio')[0],
+countdown, timerStopped=true, minutes, seconds;
 
-	startButton=body.appendChild(document.createElement('button')),
-	stopButton=body.appendChild(document.createElement('button')),
-	continueButton=body.appendChild(document.createElement('button')),
-	clearButton=body.appendChild(document.createElement('button')),
-
-	startButtonText=document.createTextNode('Старт'),
-	stopButtonText=document.createTextNode('Стоп'),
-	continueButtonText=document.createTextNode('Продовжити'),
-	clearButtonText=document.createTextNode('Cкинути'),
-
-	timerBody=body.appendChild(document.createElement('div')),
-	timerDisplay=document.createTextNode('00:00'),
-	tick, 
-	timerStopped=true, 
-	timeMinutes, 
-	timeSeconds,
-	timerTextInside=document.createTextNode('before explosion'),
-	timerText=document.createElement('div');
-
-timerBody.appendChild(timerDisplay);
-timerBody.style.height='48vh';
-timerBody.style.padding='5vh';
-timerBody.style.margin=0;
-timerBody.style.textAlign='center';
-timerBody.style.backgroundColor='red';
-timerBody.style.fontSize='20vh';
-timerBody.style.color='white';
-
-requestParagraph.appendChild(requestText);
-requestParagraph.style.display='block';
-requestParagraph.style.textAlign='center';
-requestParagraph.style.height='3vh';
-requestParagraph.style.lineHeight='5vh';
-
-minutesUnit.appendChild(minutesField);
-minutesUnit.appendChild(minutesUnitText);
-minutesUnit.style.display='block';
-minutesUnit.style.textAlign='center';
-minutesUnit.style.backgroundColor='green';
-minutesUnit.style.height='8vh';
-minutesUnit.style.padding=0;
-minutesUnit.style.margin=0;
-
-
-minutesField.style.margin='2vh';
-minutesField.value='Введіть ціле число';
-
-minutesField.onclick=function (){
+minutesField.onclick=function() {
 	minutesField.value='';
+	minutesField.placeholder='';
 }
-minutesField.onblur=function (){
-	if (minutesField.value=='')
-		minutesField.value='Введіть ціле число';
-}
-
-secondsUnit.appendChild(secondsField);
-secondsUnit.appendChild(secondsUnitText);
-secondsUnit.style.display='block';
-secondsUnit.style.textAlign='center';
-secondsUnit.style.backgroundColor='yellow';
-secondsUnit.style.height='8vh';
-secondsUnit.style.padding=0;
-secondsUnit.style.margin=0;
-
-secondsField.style.margin='2vh';
-secondsField.value='Введіть ціле число';
-
-secondsField.onclick=function (){
+secondsField.onclick=function() {
 	secondsField.value='';
+	secondsField.placeholder='';
 }
-secondsField.onblur=function (){
+minutesField.onblur=function() {
+	if (minutesField.value=='')
+		minutesField.placeholder='Введіть ціле число';
+}
+secondsField.onblur=function() {
 	if (secondsField.value=='')
-		secondsField.value='Введіть ціле число';
+		secondsField.placeholder='Введіть ціле число';
 }
-
-startButton.appendChild(startButtonText);
-startButton.style.display='block';
-startButton.style.margin='1vh auto';
-startButton.style.height='4vh';
-
-stopButton.appendChild(stopButtonText);
-stopButton.style.display='none';
-stopButton.style.margin='1vh auto';
-stopButton.style.height='4vh';
-
-continueButton.appendChild(continueButtonText);
-continueButton.style.display='none';
-continueButton.style.margin='1vh auto';
-continueButton.style.height='4vh';
-
-clearButton.appendChild(clearButtonText);
-clearButton.style.display='block';
-clearButton.style.margin='1vh auto';
-clearButton.style.height='4vh';
-
-timerText.appendChild(timerTextInside);
-
-function time() {
-	if(timeMinutes+timeSeconds>=0) {
-		timerBody.textContent='';
-		if (timeMinutes<10 && timeSeconds<10) {
-			timerDisplay=document.createTextNode('0'+timeMinutes+':'+'0'+timeSeconds);
+function validate() {
+	if (    isNaN(minutes) || minutes%1!=0 || minutesField.value=='' ||
+		isNaN(seconds) || seconds%1!=0 || secondsField.value=='' ||
+		(seconds==0 && minutes==0)||seconds<0||minutes<0) {
+		alert ('Введіть цілі числа!');
+		return false;
+	} else if  (minutes>3) {
+		alert ('Не більше чотирьох хвилин!');
+		return false;
+	} else if (seconds>59) {
+		alert ('Число секунд не може бути більше 59!');
+		return false;
+	} else return true;
+}
+function boom() {
+	var boom=document.createElement('img');
+	boom.setAttribute('src','boomPicture.gif');
+	timerBody.style.display='none';
+	timerText.style.display='none';
+	body.appendChild(boom);
+	boomSound.play();
+}
+function stopBoom() {
+	var boom=document.getElementsByTagName('img')[0];
+	startButton.style.display='block';
+	stopButton.style.display='none';
+	timerBody.style.height='50vh';
+	timerBody.style.display='block';
+	body.removeChild(boom);
+	boomSound.pause();
+	boomSound.currentTime=0.0;
+}
+function count() {
+	if(minutes>=0 && seconds>=0) {
+		if (minutes<10 && seconds<10) {
+			timerBody.textContent='0'+minutes+':'+'0'+seconds;
 		}
-		else if (timeSeconds<10) {
-			timerDisplay=document.createTextNode(timeMinutes+':'+'0'+timeSeconds);
+		else if (seconds<10) {
+			timerBody.textContent=minutes+':'+'0'+seconds;
 		}
-		else if (timeMinutes<10) {
-			timerDisplay=document.createTextNode('0'+timeMinutes+':'+timeSeconds);
+		else if (minutes<10) {
+			timerBody.textContent='0'+minutes+':'+seconds;
 		}
 		else {
-			timerDisplay=document.createTextNode(timeMinutes+':'+timeSeconds);
+			timerBody.textContent=minutes+':'+seconds;
 		}
-		timerBody.appendChild(timerDisplay);
-		timerBody.appendChild(timerText);
-		timerText.style.display='block';
-
-		if (timeSeconds==0) {
-			timeSeconds=59;
-			timeMinutes--; 
+		if (seconds==0) {
+			seconds=59;
+			minutes--; 
 		}
-		else timeSeconds--;
-	}
-	if(timerDisplay.textContent=='00:00') {
-			clearInterval(tick);
-			var boom=document.createElement('img'),
-				boomSound=new Audio('boomSound.mp3');
-			boom.setAttribute('src','boomPicture.gif');
-			boom.style.height='60vh';
-			setTimeout(function(){
-				//body.appendChild(boomSound);
-				boomSound.play();
-				body.removeChild(timerBody);
-				body.appendChild(boom);
-				timerBody.removeChild(timerText);
-				startButton.style.display='block';
-				stopButton.style.display='none';
-				continueButton.style.display='none';
-				timerStopped=true;
-			},1000);
-
-			setTimeout(function(){
-				body.removeChild(boom);
-				boomSound.pause();
-				boomSound.currentTime=0.0;
-				body.appendChild(timerBody);
-				startButton.style.display='block';
-				stopButton.style.display='none';
-				continueButton.style.display='none';
-				timerStopped=true;
-			},5000);
-		}
-
-}
-function timerRun(){
-	
-	var minutes=Number(minutesField.value), seconds=Number(secondsField.value);
-	if (isNaN(minutes) 					|| 
-		isNaN(seconds) 					|| 
-		minutes%1!=0 					|| 
-		seconds%1!=0					||
-		minutesField.value==''			||
-		secondsField.value==''			||
-		seconds=='Введіть ціле число'  	||
-		minutes=='Введіть ціле число'	||
-		seconds=='0' && minutes=='0' 		) {
-		alert ('Введіть цілі числа!');
-	}
-	else if  (minutes>3) {
-		alert ('Не більше чотирьох хвилин!');
-	}
-	else if (seconds>59) {
-		alert ('Число секунд не може бути быльше 59!');
-	}
-	else if (timerStopped){
-		timeMinutes=minutes;
-		timeSeconds=seconds;
-		tick=setInterval(time,1000);
-
-		startButton.style.display='none';
-		stopButton.style.display='block';
-		continueButton.style.display='block';
-		timerStopped=false;
-	}
-}
-function timerStop() {
-	if (!timerStopped) {
-		clearInterval(tick);
-		stopButton.style.display='none';
+		else seconds--;
+	} else {
+		timerBody.textContent='00:00';
+		clearInterval(countdown);
+		setTimeout(boom,1000);
+		setTimeout(stopBoom,5000);
 		timerStopped=true;
 	}
 }
-function timerContinue() {
+function startCountdown() {
 	if (timerStopped) {
-		tick=setInterval(time,1000);
+		minutes=Number(minutesField.value);
+		seconds=Number(secondsField.value);
+		if (validate()) {
+			startButton.style.display='none';
+			stopButton.style.display='block';
+			continueButton.style.display='none';
+			clearButton.style.display='none';
+			timerStopped=false;
+			countdown=setInterval(count,1000);
+			timerText.style.display='block';
+			timerBody.style.height='25vh';
+		}
+	}
+}
+function stopCountdown() {
+	if (!timerStopped) {
+		continueButton.style.display='block';
+		stopButton.style.display='none';
+		clearButton.style.display='block';
+		clearInterval(countdown);
+		timerStopped=true;
+	}
+}
+function continueCountdown() {
+	if (timerStopped) {
+		startButton.style.display='none';
 		stopButton.style.display='block';
+		clearButton.style.display='none';
+		continueButton.style.display='none';
+		countdown=setInterval(count,1000);
 		timerStopped=false;
 	}
 }
-function timerClear() {
-	clearInterval(tick);
-	timerBody.textContent='00:00';
-	timerStopped=true;
-	startButton.style.display='block';
-	stopButton.style.display='none';
-	continueButton.style.display='none';
+function clearCountdown() {
+	if (timerStopped) {
+		startButton.style.display='block';
+		stopButton.style.display='none';
+		clearButton.style.display='none';
+		continueButton.style.display='none';
+		clearInterval(countdown);
+		timerBody.textContent='00:00';
+		timerText.style.display='none';
+		timerBody.style.height='50vh';
+		timerStopped=true;
+	}
 }
-startButton.addEventListener('click', timerRun,false);
-stopButton.addEventListener('click', timerStop,false);
-continueButton.addEventListener('click', timerContinue,false);
-clearButton.addEventListener('click', timerClear,false);
-
+startButton.addEventListener('click', startCountdown,false);
+stopButton.addEventListener('click', stopCountdown,false);
+continueButton.addEventListener('click', continueCountdown,false);
+clearButton.addEventListener('click', clearCountdown,false);
